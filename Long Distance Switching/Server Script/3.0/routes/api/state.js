e3c22351a81b7f5ad3affa9ae14c10040c44d6fd
Mohan Raj <Mohan-Raj-Codecs@users.noreply.github.com>
2022-03-12 23:02:29 +0530

    Update state.js

--- Long Distance Switching/Server Script/3.0/routes/api/state.js
+++ Long Distance Switching/Server Script/3.0/routes/api/state.js
@@ -4,7 +4,20 @@ const fs = require('fs');
 var router = express.Router();
 
 var fileName="public/data.json";
+var on_data='{"on":true}';
+var off_data='{"on":false}';
 
+function off_write(){ //Writes Off_Data to API | returns None
+    fs.writeFile(fileName,off_data , (err) => {
+        if (err) throw err;
+    });
+}
+
+function on_write(){ //Writes On_Data to API | returns None
+    fs.writeFile(fileName,on_data , (err) => {
+        if (err) throw err;
+    });
+}
 function fetch_data(){  //Fetch Live data from File
 	try{
 	var jsonString = fs.readFileSync(fileName);
@@ -18,6 +31,16 @@ function fetch_data(){  //Fetch Live data from File
 
 router.get("/data",function(req,res){
 	res.send(fetch_data());
+});
+
+router.get("/flip_on",function(req,res){
+	on_write();
+	res.send("200");
+});
+
+router.get("/flip_off",function(req,res){
+	off_write();
+	res.send("200");
 });
 
 module.exports = router;
